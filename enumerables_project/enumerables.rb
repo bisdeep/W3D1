82a97a80a74354fd916db988e479c334e3a67c9b
bisdeep <[deepbiswasrepo@gmail.com]>
2021-06-28 17:16:48 -0400

    first try

--- enumerables_project/enumerables.rb
+++ enumerables_project/enumerables.rb
@@ -1,1 +1,19 @@
-puts "enum"
+class Array
+    def my_each(&block)
+        self.map! {|ele| block.call(ele) }
+    end
+
+    def my_select(&block)
+        newArr = []
+        newArr << block.call(self.my_each(&block))
+        return newArr
+    end
+
+end
+
+# array1 = [1,2,3,4,5,6]
+# p array1.object_id
+# p array1.my_select() {|ele| ele.even?}
+# p array1.my_each() {|ele| ele.odd?}
+# p array1.object_id
+
